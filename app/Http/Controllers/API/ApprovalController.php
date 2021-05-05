a8dbac0c43364212d1475d91c0841c01b32f7b06
ilhamgusti <ilham.wibowo51@gmail.com>
2021-05-05 07:55:54 +0700

    fix approval

--- app/Http/Controllers/API/ApprovalController.php
+++ app/Http/Controllers/API/ApprovalController.php
@@ -14,37 +14,37 @@ use Illuminate\Support\Facades\Log;
 
 class ApprovalController extends Controller
 {
-        /**
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
     {
-        if ($request->user()->tipe !== 2 || $request->user()->tipe !== 'Admin') {
+        if ($request->user->tipe !== 2 || $request->user->tipe !== 'Admin') {
             return response()->json([
                 'message' => 'Kamu tidak dapat akses untuk melihat list panti yang belum di verifikasi'
             ], 403);
         }
-        if($request->has('pageSize')){
-           $data = Panti::where('isVerified_ktp',0)->orWhere('isVerified_sertifikat',0)->paginate($request->has('pageSize'));
-        }else{
-            $data = Panti::where('isVerified_ktp',0)->orWhere('isVerified_sertifikat',0)->get();
+        if ($request->has('pageSize')) {
+            $data = Panti::where('isVerified_ktp', 0)->orWhere('isVerified_sertifikat', 0)->paginate($request->has('pageSize'));
+        } else {
+            $data = Panti::where('isVerified_ktp', 0)->orWhere('isVerified_sertifikat', 0)->get();
         }
 
         return PantiResource::collection($data);
     }
 
-       /**
+    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateApprovalRequest $request,$id)
+    public function update(UpdateApprovalRequest $request, $id)
     {
-        if ($request->user()->tipe !== 2 || $request->user()->tipe !== 'Admin') {
+        if ($request->user->tipe !== 2 || $request->user->tipe !== 'Admin') {
             return response()->json([
                 'message' => 'Kamu tidak dapat akses untuk verifikasi panti'
             ], 403);
@@ -52,7 +52,7 @@ class ApprovalController extends Controller
 
         DB::beginTransaction();
         try {
-            $panti = PantiTransformer::toInstance($request->validated(), Panti::where('id',$id)->firstOrFail());
+            $panti = PantiTransformer::toInstance($request->validated(), Panti::where('id', $id)->firstOrFail());
             $panti->save();
             DB::commit();
         } catch (Exception $ex) {
@@ -62,11 +62,11 @@ class ApprovalController extends Controller
         }
 
         return (new PantiResource($panti))
-        ->additional([
-            'meta' => [
-                'success' => true,
-                'message' => "Panti verified"
-  ]
-        ]);
+            ->additional([
+                'meta' => [
+                    'success' => true,
+                    'message' => "Panti verified"
+                ]
+            ]);
     }
 }
