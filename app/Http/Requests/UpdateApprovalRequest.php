2403ffd63dc8089831e45c4e084d6c9a82632894
ilhamgusti <ilham.wibowo51@gmail.com>
2021-08-09 23:12:38 +0700

    fix keterangan

--- app/Http/Requests/UpdateApprovalRequest.php
+++ app/Http/Requests/UpdateApprovalRequest.php
@@ -27,6 +27,7 @@ class UpdateApprovalRequest extends FormRequest
             'isVerified_ktp' => 'sometimes|required|boolean',
             'isVerified_sertifikat' => 'sometimes|required|boolean',
             'status' => 'sometimes|integer',
+            'keterangan'=> 'sometimes|integer'
         ];
     }
 }
